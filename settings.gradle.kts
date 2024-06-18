d2ca5078ddcc54c8569d8644e343ce305518e93f
“konstantin” <pon4ik2282211@gmail.com>
2024-06-18 21:57:20 +0300

    Project created, modules

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "EasyScrollPicker"
+include(":sample")
+include(":easyscrollpicker")
